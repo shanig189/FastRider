25570e840b18d945388531fe36b25f5f6be0af4e
shanigreidy <shanig190@gmail.com>
2020-01-19 17:33:52 +0200

    add tests

---
+++ README.md
@@ -0,0 +1,6 @@
+# To start the project run:
+### 1) npm install
+### 2) npm start
+
+# To start the tests run:
+### npm test
--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
-import compare from '../helpers/compare';
+import { isValidPinCode } from '../helpers/validations';
 
 it('App renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
-it("Sort atrists by artist name", () => {
-  const artists = [
-    {artistName: 'Madonna'},
-    {artistName: 'Elton'},
-    {artistName: 'Freddy'},
-    {artistName: 'Paul'},
-  ];
-
-  artists.sort(compare.bind(null, 'artistName'));
-
-  expect(artists).toEqual([
-    {artistName: 'Elton'},
-    {artistName: 'Freddy'},
-    {artistName: 'Madonna'},
-    {artistName: 'Paul'},
-  ]);
+it("Check pin validation", () => {
+  let pin = 'JN-3117-5100-LH';
+  expect(isValidPinCode(pin)).toBe(true);
+  pin = 'JN-3117-5100-L';
+  expect(isValidPinCode(pin)).toBe(false);
+  pin = 'JN-5223-5100-LH';
+  expect(isValidPinCode(pin)).toBe(false);
 });
