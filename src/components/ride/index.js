e45b1d43da051baf8a2293933b65106fa5f20fcd
shanigreidy <shanig190@gmail.com>
2020-01-24 22:14:15 +0200

    useRef instead document.getElementById!!

--- src/components/ride/index.js
+++ src/components/ride/index.js
@@ -11,7 +11,6 @@ let previosId = null;
 const Ride = ({ rideData, onSelectRide }) => {
     const { id, name, remaining_tickets, return_time } = rideData;
     const rideDataZone = { name: '', color: ''};
-    const idRef = useRef();
 
     if(rideData.zone){
         const { name, color } = rideData.zone;
@@ -20,13 +19,11 @@ const Ride = ({ rideData, onSelectRide }) => {
     }
 
     const handleRideClick = (e, color, id) => {
-        console.log(ridesIdsRefsList, "aaa")
         if(isFirstTimeClick){
             isFirstTimeClick = false;
             previosId = id;
         }else{
             if(previosId !== id){
-                // document.getElementById(previosId).style.background = '#373737';
                 ridesIdsRefsList[previosId].style.background = '#373737';
                 previosId = id;
             }
@@ -40,7 +37,7 @@ const Ride = ({ rideData, onSelectRide }) => {
     }
 
     return(
-        <div className="ride_ctn" style={ride_zone_color} id={id} ref={(idRef) => ridesIdsRefsList[id] = idRef} onClick={(e) => {handleRideClick(e, rideDataZone.color, id)}}>
+        <div className="ride_ctn" style={ride_zone_color} id={id} ref={(ref) => ridesIdsRefsList[id] = ref} onClick={(e) => {handleRideClick(e, rideDataZone.color, id)}}>
            <span className="zone_name">{rideDataZone.name}</span>
            <h4 className="ride_name">{name}</h4>
            <div className="time_Ticket_Ctn">
--- src/components/rides/index.js
+++ src/components/rides/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import useMobileDetect from 'use-mobile-detect-hook';
 import { useHistory } from 'react-router-dom';
 import { getPinCode, setPinCode } from '../../services/pinActions';
@@ -14,16 +14,19 @@ const Rides = () => {
     const [pin, setPin] = useState(getPinCode());
     const [selectedRideId, setSelectedRideId] = useState(null);
     const [message, setMessage] = useState('');
+    let ridesDataCtnRef = useRef();
+    let submitBtnRef = useRef();
+    let messageRef = useRef();
 
     const handleMobile = () => {
         document.addEventListener('touchmove', () => {
-            const ridesDataCtn = document.getElementById('ridesDataCtn');
-            const submitBtn = document.getElementById('submitBtn');
-            if(isElementAtTop(ridesDataCtn)){
-                submitBtn.style.visibility = 'visible';
-            }else{
-                submitBtn.style.visibility = 'hidden';
-                
+            if(ridesDataCtnRef.current && submitBtnRef.current){
+                if(isElementAtTop(ridesDataCtnRef.current)){
+                    submitBtnRef.current.style.visibility = 'visible';
+                }else{
+                    submitBtnRef.current.style.visibility = 'hidden';
+                    
+                }
             }
         })
     }
@@ -43,6 +46,17 @@ const Rides = () => {
         };
         fetchData();
     }, []);
+
+    useEffect(() => {
+       if(message !== '' && messageRef.current){
+        const elementTop = messageRef.current.offsetTop;
+        const body = document.body; 
+        const html = document.documentElement;
+        
+        body.scrollTop = html.scrollTop = elementTop; 
+       }
+
+    }, [message]);
 
     const handleSelectRide = (rideId) => {
         setSelectedRideId(rideId);
@@ -71,13 +85,13 @@ const Rides = () => {
         <div className="rides_ctn" >
             {
                 message && 
-                (<div style={{color: '#FF5722'}}>{message}</div>)
+                (<div style={{color: '#FF5722'}} ref={messageRef}>{message}</div>)
             }
             <form onSubmit={handleSubmit}>
                 <input className="submit_input" type="text" placeholder="#PIN" value={pin} onChange={(e) => { setPin(e.target.value); setMessage(''); }}/>
-                <button className="submit_btn" id="submitBtn">SUBMIT</button> 
+                <button className="submit_btn" id="submitBtn" ref={submitBtnRef}>SUBMIT</button> 
             </form>
-            <div className="rides_data_ctn" id="ridesDataCtn">
+            <div className="rides_data_ctn" id="ridesDataCtn" ref={ridesDataCtnRef}>
                 {ridesData}
             </div>
         </div>
